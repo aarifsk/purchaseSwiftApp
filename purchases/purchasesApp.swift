c1c6b4e3a48a23726ca7bfcdec59d76da53f94ee
shaikhaarif <aarifsk1618@gmail.com>
2024-02-23 04:08:27 +0900

    Initial Commit

---
+++ purchases/purchasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  purchasesApp.swift
+//  purchases
+//
+//  Created by Aarif Shaikh on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct purchasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
